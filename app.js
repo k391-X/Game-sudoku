f2b805fa126c7ff2501550ca7403fc2b8ca6e36b
thang <cmthang2407@gmail.com>
2022-07-01 11:07:17 +0700

    update success gif

--- app.js
+++ app.js
@@ -8,20 +8,28 @@ var sudokuApp = new Vue({
         isGameStarted: false,
         showAnswer: false,
         isNewGame: true,
+        getLevel: 0, 
+        mt: [],
     },
 
 	//Day anh chien thang va that bai vao
     mounted() {
         // Preload image
         new Image().src = 'fail.gif';
-        new Image().src = 'success.gif';
+        new Image().src = 'successEasy.gif';
+        new Image().src = 'successNormal.gif';   
+        new Image().src = 'successHard.gif';
+        new Image().src = 'successDestroy.gif';
+
     },
 
 	//Bat dau viet ham
     methods: {
 		//Ham Tao Game truyen vao do kho lay tu form index(bien level)
         initializeGame(level) {
-            var defaultSudokuMatrix = generate();
+            this.getLevel = level;
+            this.mt = generate();
+            var defaultSudokuMatrix = this.mt.result;
 
             // Empty random cells per row //De trong ngau nhien cac o trong 1 hang 
             for (var i = 0; i < defaultSudokuMatrix.length; ++i) {
@@ -41,13 +49,27 @@ var sudokuApp = new Vue({
             this.isNewGame = false;
         },
 
+        check()
+        {
+            this.sudokuMatrix = this.mt.sudoku;
+            this.isGameStarted = true;
+            this.isNewGame = false;
+        },
 	//Ham kiem tra dap an da dung chua
         evaluateGame() {
             var result = sudoku.map((x, i) => sudoku[i].map(y => parseInt(y.num)));
             var maxtrixCheck = this.sudokuMatrix.map((x, i) => this.sudokuMatrix[i].map(y => parseInt(y.num)));
 
             if (JSON.stringify(result) == JSON.stringify(maxtrixCheck)) {
-                this.answerImage = "success.gif";
+                if ( this.getLevel == 3 )
+                    this.answerImage = "successEasy.gif";
+                else if ( this.getLevel == 4 )
+                    this.answerImage = "successNormal.gif";
+                else if ( this.getLevel == 5 )
+                    this.answerImage = "successHard.gif";
+                else if ( this.getLevel == 6 )
+                   this.answerImage = "successDestroy.gif";
+
                 this.showAnswer = true;
                 this.isGameStarted = false;
                 this.isNewGame = false;
@@ -56,7 +78,7 @@ var sudokuApp = new Vue({
                     this.showAnswer = false;
                     this.isGameStarted = true;
                     this.isNewGame = false;
-                }, 2000);
+                }, 4000);
             }
             else {
                 this.answerImage = "fail.gif";
